39a17c1beeb7573277607d6b2067929a7536c8c3
Anuradha <anuradha@192-168-1-107.tpgi.com.au>
2020-04-11 22:15:37 +1000

    Updates

---
+++ precache-manifest.87bbd4c7f4868bb38029005ae3b55ebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1333b7cabad0e5fba164968fa89a955",
+    "url": "/covid19-data/index.html"
+  },
+  {
+    "revision": "83e17fd0d42088ea955d",
+    "url": "/covid19-data/static/css/main.777be535.chunk.css"
+  },
+  {
+    "revision": "d6a30b8fab0bf35775c0",
+    "url": "/covid19-data/static/js/2.2a5eced5.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid19-data/static/js/2.2a5eced5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e17fd0d42088ea955d",
+    "url": "/covid19-data/static/js/main.dc0dd7a4.chunk.js"
+  },
+  {
+    "revision": "ab07af977a05c913ec20",
+    "url": "/covid19-data/static/js/runtime-main.a7be2102.js"
+  },
+  {
+    "revision": "3fdb664f4542233cefcd4d630f074210",
+    "url": "/covid19-data/static/media/images.3fdb664f.jpeg"
+  }
+]);
